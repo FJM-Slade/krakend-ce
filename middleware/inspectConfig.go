25fa8dc23f34ecc73004a2650a59e974f094aec5
Filipe Miranda <filipe.miranda@i2s.pt>
2021-05-11 16:52:48 +0100

    New script to run in mac; remove binaries and certificates

---
+++ middleware/inspectConfig.go
@@ -0,0 +1,27 @@
+package middleware
+
+// import (
+// 	"strconv"
+
+// 	config "github.com/devopsfaith/krakend/config"
+// 	proxy "github.com/devopsfaith/krakend/proxy"
+
+// 	"fmt"
+// )
+
+var timee int = 20
+
+// func InspectConfig(c *gin.Context, MYCONFIG config.ExtraConfig){
+
+// }
+// HeaderLogs appends logging related headers to request
+// func InspectConfig(myCfg config.ExtraConfig) proxy.Middleware {
+
+// 	fmt.Println("\n -----------Int Config Value :", strconv.FormatInt(int64(timee), 10))
+
+// 	return func(next ...proxy.Proxy) proxy.Proxy {
+// 		// do things with myCfg
+// 		return
+// 	}
+
+// }
---
+++ scripts/run_macos.sh
@@ -0,0 +1,18 @@
+# !/bin/bash
+
+# SET GOPATH AND GO BIN
+currentFolder=$(pwd)
+parent=$(dirname "$currentFolder")
+gopath=$(dirname "$parent")
+
+export GOPATH="$gopath" &&
+export GOBIN=$GOPATH/bin &&
+PATH=$PATH:$GOPATH:$GOBIN &&
+export PATH &&
+
+    echo "Running in MAC-OS..."
+
+
+    # RUN FOR MACOS
+    export FC_ENABLE=1 && export FC_PARTIALS=$currentFolder/config/partials && export GODEBUG="x509ignoreCN=0" && $currentFolder/binaries/integration-hub run -c  $currentFolder/config/krakend.json
+
